421bdea4f61af91f4d014a0176f7e157a52e79df
Ilian BEN AMAR <ilian20000@hotmail.fr>
2026-02-25 18:28:11 +0100

    bruah

---
+++ src/digger_agent.py
@@ -0,0 +1,13 @@
+
+
+"""
+DICT QUERY PARAMETERS :
+{
+    difficulty: float, (min= 0, max= 1)
+    preferences: [str], (genres imdb, langage, etc...)
+    game_mode: str, (pitch des règles actuelles du jeu)
+}
+"""
+
+def query_infos(parameters):
+    return ""
---
+++ src/game_master_agent.py
@@ -0,0 +1,9 @@
+
+
+
+def main():
+    print("Salut à tous les amis, c'est David Lafarge pokémon")
+    return
+
+if __name__ == "__main__":
+    main()
