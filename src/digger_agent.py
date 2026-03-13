e29088b0e00c76004f58ddf5540d0c3efab861e0
Paul.E <paul.enjalbert@imt-atlantique.net>
2026-03-13 10:09:54 +0100

    first version

--- src/digger_agent.py
+++ src/digger_agent.py
@@ -1,5 +1,3 @@
-
-
 """
 DICT QUERY PARAMETERS :
 {
@@ -9,5 +7,122 @@ DICT QUERY PARAMETERS :
 }
 """
 
+import os
+import requests
+import random
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+from pydantic import BaseModel, Field
+
+from digger_agent_classes import *
+
+load_dotenv()
+
+def fetch_imdb_data(options : dict):
+    url = "https://api.imdbapi.dev/titles"
+    if(len(options) > 0):
+        url += "?"
+    for opt in options.keys():
+        if isinstance(options[opt], list):
+            if len(options[opt]) > 0:
+                e = random.choice(options[opt])
+                url += opt + "=" + e + "&"
+        else:
+            url += opt + "=" + options[opt] + "&"
+    url += "&languageCodes=fr"
+    print("API call to Imdb : ", url)
+    response = requests.get(url)
+    data = response.json()
+    
+    return data
+
+def get_structured_api_options(model, prompt):
+    messages = [
+        HumanMessage(content=prompt),
+    ]
+    
+    result1 = model.invoke(
+        prompt
+    )
+
+    return result1.model_dump()
+
+"""Take a response of an api call
+Return the full description of a film"""
+def choose_artwork(data):
+    choice = random.choice(data["titles"])
+    url = "https://api.imdbapi.dev/titles/" + choice["id"]
+    print("API call to Imdb : ", url)
+    response = requests.get(url)
+    data = response.json()
+
+    #url += "/akas"
+    #for 
+
+    return data
+
+def get_info(model, data):
+    messages = [
+        SystemMessage("""You have to sum up all the informations contains in the following
+                        dictionnary, which contains all the informations on a given artwork
+                      Focus on factual informations, such as directors, main actors, year, plot, name.
+                      """),
+        HumanMessage(content=str(data)),
+    ]
+    temp_response = model.invoke(messages)
+    print(temp_response.content)
+
+    messages = [
+        SystemMessage("""Ask a unique random question about the following description of an artwork
+                      , Focus on factual informations, such as directors, main actors, year, plot, name"""),
+        HumanMessage(content=temp_response.content),
+    ]
+
+    agent = create_agent(
+        model,
+        tools=[randomTool],
+    )
+
+    final_response = agent.invoke({"messages": messages})
+    print(final_response["messages"][-1].content)
+
+
+
 def query_infos(parameters):
-    return ""
+    model = ChatOpenAI(
+        model=os.getenv("AI_MODEL"),
+        base_url=os.getenv("AI_ENDPOINT"),
+        api_key=os.getenv("AI_API_KEY"),
+    )
+
+    structured_model = model.with_structured_output(Artworks_Informations)
+
+    prompt = """
+    Un film de science fiction
+    """
+    options = get_structured_api_options(structured_model, prompt)
+
+    data = fetch_imdb_data(options)
+    artwork = choose_artwork(data)
+    print(artwork)
+
+    response = get_info(model, artwork)
+
+    return response
+
+def main():
+    param = {
+        "difficulty" : 0.0,
+        "preferences" : [],
+        "game_mode" : "Find a info about the date of a film"
+    }
+    query_infos(param)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/digger_agent_classes.py
@@ -0,0 +1,35 @@
+import random
+
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+
+from pydantic import BaseModel, Field
+from typing import List, Literal
+from enum import Enum
+import sys
+class RandomInput(BaseModel):
+    """Input for random choice in a list of string"""
+
+    strList : List[str] = Field(description="The list of string where a random choice has to be performed")
+
+@tool(args_schema=RandomInput)
+def randomTool(strList: List[str]) -> str:
+    """Perform a random choice in a list of string"""
+
+    print("tool call to random on : ")
+    return random.choice(strList)
+
+
+class Artworks_Informations(BaseModel):
+    """General Informations about a bunch of artwork"""
+
+    types : List[Literal[ "MOVIE", "TV_SERIES", "TV_MINI_SERIES", "TV_SPECIAL", "TV_MOVIE", "SHORT", "VIDEO", "VIDEO_GAME"]] = Field(description="The list of artworks types, empty if none are specified")
+    genres : List[Literal[ "Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film-Noir", "Game-Show", "History", "Horror", "Music", "Musical", "Mystery", "News", "Reality-TV", "Romance", "Sci-Fi", "Short", "Sport", "Talk-Show", "Thriller", "War", "Western" ]] = Field(description="The list of artworks genres, empty if none are specified")
+    #countryCodes : List[str] = Field(description="The ISO 3166-1 code of countries artworks came from, empty if none are specified, perform a random choice when there is a list of country")
+    #languageCodes : str = Field(description="The ISO 639-1 or ISO 639-2 code of countries artworks came from, put only one in the list if there is the choici between several, empty if none are specified")
+    startYear: str = Field(description="The lower limit for artworks creation year, put 1800 if there is no upper limit")
+    endYear: str = Field(description="The upper limit for artworks creation year, put 2100 if there is no upper limit")
